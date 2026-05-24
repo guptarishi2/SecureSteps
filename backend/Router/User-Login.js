f7810d293f3ee6e7a89aebf82d29f942aa64e0ed
guptarishi2 <160143714+guptarishi2@users.noreply.github.com>
2026-05-25 02:39:25 +0530

    second commit

--- backend/Router/User-Login.js
+++ backend/Router/User-Login.js
@@ -1,3 +1,4 @@
+// require("dotenv").config();
 const Router = require("express");
 const UserModel = require("../Model/UserSchema");
 const LoginRouter = Router();
@@ -8,12 +9,12 @@ LoginRouter.post("/user-login" , async(req , res)=>{
     if(!newUser){
         return res.status(404).json("please Register First")
     }
-    res.clearCookie("token", "26020451202", { path: "/", domain: "localhost", httplOnly: true, signed: true })
+    res.clearCookie("token", process.env.JWT_SECRET, { path: "/", domain: "localhost", httplOnly: true, signed: true })
     const expires = new Date()
     expires.setDate(expires.getDate() + 7)
     const payload = {newUser};
-    const token = jwt.sign({payload} , "26020451202")
-    res.cookie("token", token, "26020451202", { path: "/", domain: "localhost", httplOnly: false, signed: true, expires })
+    const token = jwt.sign({payload} , process.env.JWT_SECRET)
+    res.cookie("token", token, process.env.JWT_SECRET, { path: "/", domain: "localhost", httplOnly: false, signed: true, expires })
     console.log(req.cookies.token);
     
     res.status(200).json(newUser)
